a3b3993d6355a27f1168cd9ae6a320d39fbf55b5
Dhanunjayarao <aradhyuladhanunjay@gamil.com>
2025-06-09 00:56:39 +0530

    sample commit

--- src/main/java/com/example/demo_app_curd/Controller/UserController.java
+++ src/main/java/com/example/demo_app_curd/Controller/UserController.java
@@ -33,7 +33,11 @@ public class UserController {
         String s1 = "abcd";
         int s=10;
         System.out.println("saving the user details in the DB");
+<<<<<<< Updated upstream
         System.out.println(d);
+=======
+        System.out.println(status);
+>>>>>>> Stashed changes
        User u=userService.savedetails(user);
        return new ResponseEntity<>(u, HttpStatus.CREATED);
     }
